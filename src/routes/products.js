686f41c4a219be08facd4c97f677b851d55956d8
solo092 <thamerown@gmail.com>
2026-08-21 07:46:38 -0700

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -10,7 +10,7 @@ const {
   getAdminProducts,
 } = require('../controllers/productController');
 const { authenticateAdmin } = require('../middleware/auth');
-const { productValidation } = require('../middleware/validate');
+const { protect } = require('../middleware/validate');
 const { upload } = require('../config/cloudinary');
 
 // Public
@@ -18,14 +18,14 @@ router.get('/', getAllProducts);
 router.get('/location/:location', getProductsByLocation);
 
 // Admin list (before :id)
-router.get('/admin/all', authenticateAdmin, getAdminProducts);
+router.get('/admin/all', protect, getAdminProducts);
 
 // Public single
 router.get('/:id', getProductById);
 
 // Admin mutations
-router.post('/', authenticateAdmin, upload.array('images', 5), productValidation, createProduct);
-router.put('/:id', authenticateAdmin, upload.array('images', 5), updateProduct);
-router.delete('/:id', authenticateAdmin, deleteProduct);
+router.post('/', protect, upload.array('images', 5), productValidation, createProduct);
+router.put('/:id', protect, upload.array('images', 5), updateProduct);
+router.delete('/:id', protect, deleteProduct);
 
 module.exports = router;
