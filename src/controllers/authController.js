f5accfdb692b829960da15fdb204bf6dd8c6680a
solo092 <thamerown@gmail.com>
2026-08-21 01:44:59 -0700

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -29,10 +29,10 @@ const login = async (req, res) => {
     );
 
     res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.json({
