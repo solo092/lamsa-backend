c0d1670a81343a423abae6439329020d2d91ca60
solo092 <thamerown@gmail.com>
2026-08-22 04:46:17 -0700

    Update validate.js

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -27,7 +27,7 @@ const orderValidation = [
     .notEmpty().withMessage('العنوان مطلوب')
     .isLength({ min: 5, max: 500 }).withMessage('العنوان لازم يكون مفصل أكثر'),
   body('product_id')
-    .isInt({ min: 1 }).withMessage('المنتج غير صحيح'),
+    .optional(), // جعل الحقل اختياري لتفادي تحويله لـ Integer
   body('size')
     .isIn(['L', 'XL', 'XXL', 'XXXL']).withMessage('المقاس غير صحيح'),
   body('quantity')
